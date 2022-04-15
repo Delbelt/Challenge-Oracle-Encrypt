486b64ce794eaad1e8498d4532ade06b45b967ef
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-04-15 17:00:56 -0300

    import functions.js [helpers] to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('index OK')
+import {getValueByClass, setValueByClass} from "./helpers/functions.js";
+
+// test implementation: functions library
+var titulo = getValueByClass(".title_page");
+setValueByClass(titulo, "Delbelt - Encrypter");
+
+console.log("Value:", titulo.textContent); // check index.js found
