93722dd76f1f151929818212c6de449e9185bf8e
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-04-15 22:01:01 -0300

    implement refactoring on fuctions.js to index.js [src]

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-import {getValueByClass, setValueByClass} from "./helpers/functions.js";
+import { addEvent, getValueByRef, setValueByRef, showConsole } from './helpers/functions.js';
 
 // test implementation: functions library
-var titulo = getValueByClass(".title_page");
-setValueByClass(titulo, "Delbelt - Encrypter");
+setValueByRef('.title_page', 'Delbelt - Encrypter');
+showConsole(getValueByRef('.title_page').textContent);
 
-console.log("Value:", titulo.textContent); // check index.js found
+setValueByRef('#txt', '123'); // test setValue
+
+//var input = getValueByRef('.textarea-encrypter').value;
+//showConsole(input);
+
+addEvent('#btn-encrypt', encrypt, 'click');
+addEvent('#btn-decrypt', decrypt, 'click');
+
+function encrypt()
+{
+    if(getValueByRef('.textarea-encrypter').value.length < 1)
+    {
+        showConsole('Did not receive any message');
+    }
+
+    else
+    {
+        //showConsole(getValueByRef('.textarea-encrypter').value.length);
+        showConsole('encrypted');
+        setValueByRef('.textarea-result', 'encrypt');
+    }
+}
+
+function decrypt()
+{
+    if(getValueByRef('.textarea-encrypter').value.length < 1)
+    {
+        showConsole('Did not receive any message');        
+    }
+
+    else
+    {
+        //showConsole(getValueByRef('.textarea-encrypter').value.length);
+        showConsole('decrypted');
+        setValueByRef('.textarea-result', 'decrypt');
+    }
+}
