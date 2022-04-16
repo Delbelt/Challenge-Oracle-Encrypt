590c1371c265ad1948d06a9c98d9357b3479e1b3
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-04-15 23:09:58 -0300

    add method get input value on functions.js [helpers]

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -6,4 +6,7 @@ const showConsole = (data, ...params) => console.log(data, ...params);
 
 const addEvent = (ref, func, event) => getValueByRef(ref).addEventListener(event, func);
 
-export { getValueByRef, setValueByRef, showConsole, addEvent };
+const getInputValue = (refInput, refOutput) => getValueByRef(refInput).addEventListener
+('keyup', (event) => setValueByRef(refOutput, event.path[0].value.length));
+
+export { getValueByRef, setValueByRef, showConsole, addEvent, getInputValue };
